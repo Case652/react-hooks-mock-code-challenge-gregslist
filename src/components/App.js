489aed412715df45883c2061d146bac22fea5e7b
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-10-10 13:30:40 -0500

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listingsData, setListings] = useState([])
+  const removeListing = doomedID => {
+    const newListings = listingsData.filter(lObj => lObj.id !== doomedID)
+    setListings(newListings)
+  }
+  useEffect(()=>{
+    fetch(`http://localhost:6001/listings`)
+    .then(r => r.json())
+    .then(setListings)
+  },[])
+
+  const [search,setSearch] = useState("")
+  const updateSearch = newValue => setSearch(newValue.toLowerCase())
+  const filteredListings = listingsData.filter(lObj =>{
+    if(lObj.description.includes(search.toLowerCase())) {
+      return true
+    } else {
+      return false
+    }
+  })
+  console.log(listingsData)
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header updateSearch={updateSearch}/>
+      <ListingsContainer listings={filteredListings} removeListing={removeListing}/>
     </div>
   );
 }
